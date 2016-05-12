3a3cb26a472a760581609c01b514bc705d0e39ab
Maxim Konovalov <komaniks@gmail.com>
2016-05-12 20:52:18 +0900

    TapeEquilibrium

---
+++ TapeEquilibrium/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void solution() throws Exception {
+        assertEquals(1, new Solution().solution(new int[]{3, 1, 2, 4, 3}));
+    }
+}
