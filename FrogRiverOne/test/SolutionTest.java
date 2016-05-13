cf6cc95f22abf832bafaae006d148ffad4b589a3
Maxim Konovalov <komaniks@gmail.com>
2016-05-13 18:48:20 +0900

    FrogRiverOne

---
+++ FrogRiverOne/test/SolutionTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @org.junit.Test
+    public void solution() throws Exception {
+        int[] data = {1, 3, 1, 4, 2, 3, 5, 4};
+        assertEquals(6, new Solution().solution(5, data));
+    }
+}
