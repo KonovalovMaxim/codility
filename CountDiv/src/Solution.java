8e36f84e9759798c28d4d42db2a2a2932e299f16
Maxim Konovalov <komaniks@gmail.com>
2016-05-15 12:54:40 +0900

    CountDiv

---
+++ CountDiv/src/Solution.java
@@ -0,0 +1,6 @@
+
+class Solution {
+    public int solution(int A, int B, int K) {
+        return B / K - (A / K) + (A % K == 0 ? 1 : 0);
+    }
+}
---
+++ CountDiv/test/SolutionTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void solution() throws Exception {
+        assertEquals(3, new Solution().solution(6, 11, 2));
+    }
+
+}
