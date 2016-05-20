0b97625111e04cad6df5851a7de1d7a6c114f25e
Maxim Konovalov <komaniks@gmail.com>
2016-05-20 19:57:48 +0900

    StoneWall

---
+++ StoneWall/test/SolutionTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void solution() throws Exception {
+        assertEquals(7, new Solution().solution(new int[]{8, 8, 5, 7, 9, 8, 7, 4, 8}));
+    }
+
+}
