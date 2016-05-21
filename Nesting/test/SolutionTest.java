274b7769b721c3debb54cc30c49635bc1fa8b33d
Maxim Konovalov <komaniks@gmail.com>
2016-05-21 14:30:11 +0900

    Nesting

---
+++ Nesting/test/SolutionTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void solution() throws Exception {
+        assertEquals(1, new Solution().solution("(()(())())"));
+    }
+
+    @Test
+    public void solution2() throws Exception {
+        assertEquals(0, new Solution().solution(")("));
+    }
+
+    @Test
+    public void solution3() throws Exception {
+        assertEquals(0, new Solution().solution("())"));
+    }
+}
