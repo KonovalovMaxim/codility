72767bfc4bc8c03d919b2d595920367a4f0c70b2
Maxim Konovalov <komaniks@gmail.com>
2016-05-21 15:00:51 +0900

    EquiLeader

---
+++ EquiLeader/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void solution() throws Exception {
+        assertEquals(2, new Solution().solution(new int[]{4, 3, 4, 4, 4, 2}));
+    }
+}
