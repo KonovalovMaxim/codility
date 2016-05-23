75f3eb5838127d812a334f0c290df588e659f748
Maxim Konovalov <komaniks@gmail.com>
2016-05-23 21:20:55 +0900

    CountFactors

---
+++ CountFactors/src/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int N) {
+        if (N <= 0) {
+            return 0;
+        }
+        if (N == 1) {
+            return 1;
+        }
+        int counter = 0;
+        for (int i = 1; i <= Math.sqrt(N); i++) {
+            if (N % i == 0) {
+                counter += 2;
+            }
+        }
+        if (Math.sqrt(N) % 1 == 0.0) {
+            counter--;
+        }
+        return counter;
+    }
+}
