73c8fb88ca9f496ec23b91b253797fd44cf3437e
Maxim Konovalov <komaniks@gmail.com>
2016-05-23 15:32:10 +0900

    MinPerimeterRectangle

---
+++ MinPerimeterRectangle/src/Solution.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public int solution(int N) {
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i * i <= N; i++) {
+            if (N % i == 0) {
+                min = Math.min(min, 2 * (i + (N / i)));
+            }
+        }
+        return min;
+    }
+}
---
+++ MinPerimeterRectangle/test/SolutionTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void solution() throws Exception {
+        assertEquals(22, new Solution().solution(30));
+    }
+
+    @Test
+    public void solution2() throws Exception {
+        assertEquals(24, new Solution().solution(36));
+    }
+
+    @Test
+    public void solution3() throws Exception {
+        assertEquals(4, new Solution().solution(1));
+    }
+
+}
