7bdaf9370c1622f9f4e06009c0478fda25077687
Maxim Konovalov <komaniks@gmail.com>
2016-06-07 02:58:26 +0900

    MinMaxDivision

---
+++ MinMaxDivision/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void solution() throws Exception {
+        assertEquals(6, new Solution().solution(3, 5, new int[]{2, 1, 5, 1, 2, 2, 2}));
+    }
+}
