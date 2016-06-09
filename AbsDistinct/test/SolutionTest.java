dfca8fc3b4cbbc5a4db9eedcd9f7fa043a4f3259
Maxim Konovalov <komaniks@gmail.com>
2016-06-10 05:42:16 +0900

    AbsDistinct

---
+++ AbsDistinct/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void testSolution() throws Exception {
+        assertEquals(5, new Solution().solution(new int[]{-5, -3, -1, 0, 3, 6}));
+    }
+}
