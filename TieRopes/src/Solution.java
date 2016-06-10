5ac358cabb72d538568d7e0a4ae6dcb5f34445ec
Maxim Konovalov <komaniks@gmail.com>
2016-06-11 05:09:17 +0900

    TieRopes

---
+++ TieRopes/src/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int solution(int K, int[] A) {
+
+        int counter = 0;
+        int sum = 0;
+        for (int i : A) {
+            sum += i;
+            if (sum >= K) {
+                sum = 0;
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ TieRopes/test/SolutionTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void solution() throws Exception {
+        assertEquals(3, new Solution().solution(4, new int[]{1, 2, 3, 4, 1, 1, 3}));
+    }
+}
